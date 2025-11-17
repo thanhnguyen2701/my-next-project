05594588b9b8fd78356f51ce9d6907675f39fd41
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-11-17 09:47:57 +0700

    Post Hog Setup

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental:{
-    turbopackFileSystemCacheForDev: true
-  }
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
